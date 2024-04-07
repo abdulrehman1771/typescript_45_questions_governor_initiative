6db177d65be40c7c934f66bc0de49702168440bb
abdulrehman1771 <hhatori476@gmail.com>
2024-04-07 11:53:59 +0500

    Complete

--- question_31/question_31.js
+++ question_31/question_31.js
@@ -1,14 +1,18 @@
-let usernames = [];
-// usernames.forEach(element => {
-//     if(usernames.length=0){
-//         console.log('We need to find some users!');
-//     } if(element==='admin'){
-//         console.log('Hello admin, would you like to see a status report?');
-//     } else if(element!='admin'){
-//         console.log(`Hello ${element}, thank you for logging in again`);   
-//     } 
-// });
-if (usernames.length === 0) {
-    console.log('We need to find some users!');
+let usernames = ['admin', 'eric', 'george', 'sam', 'fred'];
+while (true) {
+    for (let a = 0; a < usernames.length; a++) {
+        if (usernames[a] === 'admin') {
+            console.log('Hello admin, would you like to see a status report?');
+            usernames.splice(a, 0);
+        }
+        else if (usernames.length === 0) {
+            console.log('We need to find some users!');
+            break;
+        }
+        else {
+            console.log(`Hello ${usernames[a]}, thank you for logging in again`);
+            usernames.splice(a, 0);
+        }
+    }
 }
 export {};
--- question_31/question_31.ts
+++ question_31/question_31.ts
@@ -1,6 +1,17 @@
-let usernames:string[]=[]
+let usernames:string[]=['admin', 'eric','george','sam','fred']
 
-if(usernames.length===0){
-    console.log('We need to find some users!');
-    
+while(true){
+    if(usernames[a]==='admin'){
+        console.log('Hello admin, would you like to see a status report?');
+        usernames.splice(a,0)
+    } 
+    else if(usernames.length===0){
+        console.log('We need to find some users!');
+        break;
+        
+    } else{
+        console.log(`Hello ${usernames[a]}, thank you for logging in again`);
+        usernames.splice(a,0)
+    }
+
 }
